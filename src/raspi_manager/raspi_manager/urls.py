5ce1380bcf5a6a0175385dff865c659a9fc97bd8
Christian Deacon <gamemann@gflclan.com>
2022-02-02 09:22:40 +0000

    Prepare static for production.

--- src/raspi_manager/raspi_manager/urls.py
+++ src/raspi_manager/raspi_manager/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 import manager.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', manager.views.manage)
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
