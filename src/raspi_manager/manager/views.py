525121a62449005038ab0eb4736b2984bfa203dc
Christian Deacon <gamemann@gflclan.com>
2022-02-02 05:58:17 +0000

    More tries.

--- src/raspi_manager/manager/views.py
+++ src/raspi_manager/manager/views.py
@@ -21,26 +21,28 @@ def manage(request):
                 print("Error starting Steam Link -> " + e + ".")
                 success = False 
 
-            if res != 0:
+            if res is None or res.returncode != 0:
                 print("Error starting Steam link => Result isn't 0 (" + str(res) + ").")
+
+                success = False
 
         elif "sl-stop" in request.POST:
             res_one = 0
             res_two = 0
 
             try:
-                res_one = os.system("pkill -9 -f steamlink")
-                res_two = os.system("pkill -9 -f streaming_client")
+                res_one = subprocess.Popen(["pkill", "-9", "-f", "steamlink"])
+                res_two = subprocess.Popen(["pkill",  "-9", "-f", "streaming_client"])
             except Exception as e:
                 print("Error stopping Steam Link => " + e + ".")
                 success = False
 
-
-            if res_one != 0 or res_two != 0:
-                if res_one != 0:
-                    print("Error stopping Steam Link => Killing Steam Link's result isn't 0 (" + str(res_one) + ").")
-                elif res_two != 0:
-                    print("Error stopping Steam Link => Killing Stream Client's result isn't 0 (" + str(res_two) + ").")
+            if res_one.returncode != 0 or res_two.returncode != 0:
+                if res_one is not None and res_one.returncode != 0:
+                    print("Error stopping Steam Link => Killing Steam Link's result isn't 0 (" + str(res_one.returncode) + ").")
+                elif res_two is not None and res_two.returncode != 0:
+                    print("Error stopping Steam Link => Killing Stream Client's result isn't 0 (" + str(res_two.returncode) + ").")
+                    
                 success = False
 
     return render(request, 'manager/index.html', {"action": action, "success": success})
